c69f227589530c1e88fb726d5326a6aadefdfc1a
Oleg Turzhanskii <104592735+olegturzhanskii@users.noreply.github.com>
2022-10-23 21:54:55 +0300

    Update test_functionality.py

--- test_functionality.py
+++ test_functionality.py
@@ -1,16 +1,22 @@
 import unittest
+import os
 from unittest.mock import patch
+
 import functionality
+
+QUIZLET_LINK = os.getenv('QUIZLET_LINK')
 
 
 class TestFunctionality(unittest.TestCase):
     def test__parse_challenges(self):
         with patch('functionality.requests.get') as mocked_get:
+            url = QUIZLET_LINK[:-4]
+
             mocked_get.return_value.ok = True
             mocked_get.return_value.text = 'Success'
             response = functionality._parse_challenges()
             mocked_get.assert_called_with(
-                'https://quizlet.com/ru/632318229/flash-cards/',
+                url,
                 headers=functionality.headers
             )
             self.assertEqual(response, 'Success')
@@ -18,29 +24,32 @@ class TestFunctionality(unittest.TestCase):
             mocked_get.return_value.ok = False
             response = functionality._parse_challenges()
             mocked_get.assert_called_with(
-                'https://quizlet.com/ru/632318229/flash-cards/',
+                url,
                 headers=functionality.headers
             )
-            self.assertEqual(response, 'Failed to access the current website.')
+            self.assertEqual(response, f'Failed to access the current website {url}.')
 
     def test__parse_word(self):
         with patch('functionality.requests.get') as mocked_get:
+            word = 'examination'
+            url = f'https://dictionary.cambridge.org/dictionary/essential-american-english/{word}'
+
             mocked_get.return_value.ok = True
             mocked_get.return_value.text = 'Success'
-            response = functionality._parse_challenges()
+            response = functionality._parse_word(word)
             mocked_get.assert_called_with(
-                'https://quizlet.com/ru/632318229/flash-cards/',
+                url,
                 headers=functionality.headers
             )
             self.assertEqual(response, 'Success')
 
             mocked_get.return_value.ok = False
-            response = functionality._parse_challenges()
+            response = functionality._parse_word(word)
             mocked_get.assert_called_with(
-                'https://quizlet.com/ru/632318229/flash-cards/',
+                url,
                 headers=functionality.headers
             )
-            self.assertEqual(response, 'Failed to access the current website.')
+            self.assertEqual(response, f'Failed to access the current website {url}.')
 
 
 if __name__ == '__main__':
